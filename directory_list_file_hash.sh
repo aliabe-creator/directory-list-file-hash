23b806d64d559ca6d5e434fe27fe2d5d7f9bd9d7
aliabe <80125711+aliabe-creator@users.noreply.github.com>
2023-05-27 12:17:00 -0700

    Update directory_list_file_hash.sh

--- directory_list_file_hash.sh
+++ directory_list_file_hash.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 find -type f -exec sh -c 'xxhsum "$1" | awk "{ print \"$1\" \",\" \$NF }"' sh {} \; | sort -t ',' -k1,1 > "$(date +"%Y_%m_%d_%I_%M_%p").checksums"
-
-if [[ $(find -type f | wc -l | awk '{print $1}') -eq $(wc -l *.checksums | awk '{print $1}') ]];
+ACTUAL_FC = `find -type f | wc -l | awk '{print $1}'`
+OUTPUT_LC = `wc -l *.checksums | awk '{print $1}'`
+echo $ACTUAL_FC $OUTPUT_FC
+if [[ $(ACTUAL_FC) -eq $(OUTPUT_LC) ]];
 then
     echo Verified same number of lines in checksums file as files in directory.
 else
