98687502df309b44e7669e1191f3349973a88cdf
aliabe <80125711+aliabe-creator@users.noreply.github.com>
2023-05-27 11:58:39 -0700

    Update directory_list_file_hash.sh

--- directory_list_file_hash.sh
+++ directory_list_file_hash.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-find -type f -exec sh -c 'xxhsum "$1" | awk "{ print \"$1\" \",\" \$NF }"' sh {} \; | sort -t ',' -k1,1
+find -type f -exec sh -c 'xxhsum "$1" | awk "{ print \"$1\" \",\" \$NF }"' sh {} \; | sort -t ',' -k1,1 > "$(date +"%Y_%m_%d_%I_%M_%p").checksums"
+if [[$(find -type f | wc -l) -eq $(wc -l *.checksums)]]
+then
+    echo Verified same number of lines in checksums file as files in directory.
+else
+    echo Failed to verify same number of lines in checksums file as files in directory.
+fi
+
