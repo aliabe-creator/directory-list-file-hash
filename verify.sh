ff23b351b4c2aca5ea5b0a39bfb4c567d3c3a4b3
aliabe <80125711+aliabe-creator@users.noreply.github.com>
2023-05-27 14:41:46 -0700

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Check for multiple files matching *checksums
-checksum_files=(*checksums)
-num_checksum_files=${#checksum_files[@]}
-
-if (( num_checksum_files > 1 )); then
-    # Sort the files by modification time in descending order
-    sorted_files=($(ls -t "${checksum_files[@]}"))
-
-    # Delete all except the newest file
-    for (( i=1; i < num_checksum_files; i++ )); do
-        rm "${sorted_files[$i]}"
-    done
-fi
-
 # Calculate the line counts and compare them
 ACTUAL_FC=$(find -type f | wc -l | awk '{print $1}')
 OUTPUT_LC=$(wc -l *.checksums | awk '{print $1}')
