6226ad7da5d6604e0309e231b453fc8d77b64349
aliabe <80125711+aliabe-creator@users.noreply.github.com>
2023-05-27 14:36:58 -0700

    Update directory_list_file_hash.sh

--- directory_list_file_hash.sh
+++ directory_list_file_hash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash -c find -type f -exec sh -c 'FN="$1" && xxhsum "$FN" | awk "{ print \"$FN\" \",\" \$1 }"' sh {} \; | sort -t ',' -k1,1 > "$(date +"%Y_%m_%d_%I_%M_%p").checksums"
+find -type f -exec sh -c 'FN="$1" && echo "$FN, $(xxhsum "$FN" | awk "{ print \$1 }")"' sh {} \; | sort -t ',' -k1,1 > "$(date +"%Y_%m_%d_%I_%M_%p").checksums"
