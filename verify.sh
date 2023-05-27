c5adc55cfb672d14c0a552b1f0b77d2792e312b4
aliabe <80125711+aliabe-creator@users.noreply.github.com>
2023-05-27 14:47:56 -0700

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -5,8 +5,8 @@ ACTUAL_FC=$(find -type f | wc -l | awk '{print $1}')
 OUTPUT_LC=$(wc -l *.checksums | awk '{print $1}')
 echo "$ACTUAL_FC", "$OUTPUT_LC"
 
-if [[ "$ACTUAL_FC" == "$OUTPUT_LC" ]]; then
-    echo "Verified same number of lines in checksums file as files in directory."
+if [[ "$ACTUAL_FC" -eq $((OUTPUT_LC + 1)) ]]; then
+    echo "Verified ACTUAL is exactly 1 greater than OUTPUT."
 else
-    echo "Failed to verify same number of lines in checksums file as files in directory."
+    echo "Failed to verify ACTUAL is exactly 1 greater than OUTPUT."
 fi
