38d1c350164150aecb3e3b3f0b95c1a1f89f4995
aliabe <80125711+aliabe-creator@users.noreply.github.com>
2023-05-27 14:50:17 -0700

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -6,7 +6,8 @@ OUTPUT_LC=$(wc -l *.checksums | awk '{print $1}')
 echo "$ACTUAL_FC", "$OUTPUT_LC"
 
 if [[ "$ACTUAL_FC" -eq $((OUTPUT_LC + 1)) ]]; then
-    echo "Verified ACTUAL is exactly 1 greater than OUTPUT."
+    # 1 greater because the actual FC will include the actual checksum file itself, but when the list program runs, it pipes to the checksum file so it does not take its hash into account.
+    echo "LGTM"
 else
-    echo "Failed to verify ACTUAL is exactly 1 greater than OUTPUT."
+    echo "Sadge, failed verification."
 fi
